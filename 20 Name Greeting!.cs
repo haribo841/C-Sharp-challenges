fe11930f482c86fd7364c47551e45250407cfeac
haribo841 <adamwaldemarkubis@gmail.com>
2022-08-18 15:33:27 +0200

    Create 20 Name Greeting!.cs
    Function that takes a name and returns a greeting in the form of a string

---
+++ 20 Name Greeting!.cs
@@ -0,0 +1,21 @@
+//Create a function that takes a name and returns a greeting in the form of a string.
+using System;
+using NUnit.Framework;
+​
+[TestFixture]
+public class Tests
+{
+    [Test]
+    [TestCase("Gerald", Result="Hello Gerald!")]
+    [TestCase("Fatima", Result="Hello Fatima!")]
+    [TestCase("Ed", Result="Hello Ed!")]
+    [TestCase("Tiffany", Result="Hello Tiffany!")]
+    public static string FixedTest(string a)
+    {
+        return Program.HelloName(a);
+    }
+}
+public class Program
+{
+		public static string HelloName(string name)=>"Hello "+a;
+}
