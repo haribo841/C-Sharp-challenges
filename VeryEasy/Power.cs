656a82c8059a3726217a113936659565ec4bd9ee
Adam Kubiś <adamwaldemarkubis@gmail.com>
2022-12-31 05:31:29 +0100

    Power with Test
    Positive integers power function with tests

---
+++ VeryEasy/Power.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace VeryEasy;
+public class Power
+{
+    public int DoPower(int a, int b)
+    {
+        if (a < 0) throw new ArgumentOutOfRangeException("a");
+        if (b < 0) throw new ArgumentOutOfRangeException("b");
+        int result = 1;
+        for (int i = 0; i < b; i++)
+        {
+            result *= a;
+        }
+        return result;
+    }
+}
