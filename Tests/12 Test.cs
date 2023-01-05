35df4f5c365ba9bfd9b610c26f8362b1a6790346
Adam Kubiś <adamwaldemarkubis@gmail.com>
2023-01-05 18:31:31 +0100

    Fix12
    Fixed test 12

---
+++ Tests/12 Test.cs
@@ -0,0 +1,17 @@
+using System;
+using NUnit.Framework;
+namespace VeryEasy.Tests
+{
+    [TestFixture]
+    public class Tests12
+    {
+        [Test]
+        [TestCase(false, true)]
+        [TestCase(true, false)]
+        public void FixedTest(bool boolean, bool expectedResult)
+        {
+            bool result = Program12.Reverse(boolean);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ VeryEasy/12 Flip the Boolean.cs
@@ -0,0 +1,6 @@
+//Create a function that reverses a boolean value.
+using System;
+public class Program12
+{
+    public static bool Reverse(bool boolean)=>!boolean;
+}
