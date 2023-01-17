90af37e9eebf26acb1edc5a18a48ed210f1097d3
Adam Kubiś <adamwaldemarkubis@gmail.com>
2023-01-17 01:57:01 +0100

    Fix15
    Fixed test 15

---
+++ Tests/15 Test.cs
@@ -0,0 +1,20 @@
+using System;
+using NUnit.Framework;
+namespace VeryEasy.Tests
+{
+    [TestFixture]
+    public class Tests15
+    {
+        [Test]
+        [TestCase(4, 8, false, TestName = "{0} is not equal to {1}")]
+        [TestCase(111, 111, true, TestName = "{0} is equal to {1}")]
+        [TestCase(2, 5, false, TestName = "{0} is not equal to {1}")]
+        [TestCase(0, 6, false, TestName = "{0} is not equal to {1}")]
+        [TestCase(10, 10, true, TestName = "{0} is equal to {1}")]
+        public void FixedTest(int a, int b, bool expectedResult)
+        {
+            bool result = Program15.IsSameNum(a, b);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ VeryEasy/15 Are the Numbers Equal.cs
@@ -0,0 +1,6 @@
+//Create a function that returns true when num1 is equal to num2; otherwise return false.
+using System;
+public class Program15
+{
+    public static bool IsSameNum(int num1, int num2)=>num1==num2;
+}
