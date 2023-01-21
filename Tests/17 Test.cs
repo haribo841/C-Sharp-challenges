9452fe9d58cca29c63b6dd110b950dbe6c103942
Adam Kubiś <adamwaldemarkubis@gmail.com>
2023-01-21 21:34:40 +0100

    Fix17
    Fixed test 17

---
+++ Tests/17 Test.cs
@@ -0,0 +1,19 @@
+using System;
+using NUnit.Framework;
+namespace VeryEasy.Tests
+{
+    [TestFixture]
+    public class Tests17
+    {
+        [Test]
+        [TestCase(true, true, true, TestName = "{0} AND {1} is equal to {2}")]
+        [TestCase(true, false, false, TestName = "{0} AND {1} is equal to {2}")]
+        [TestCase(false, true, false, TestName = "{0} AND {1} is equal to {2}")]
+        [TestCase(false, false, false, TestName = "{0} AND {1} is equal to {2}")]
+        public void And(bool x, bool y, bool expectedResult)
+        {
+            bool result = Program17.And(x, y);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ VeryEasy/17 Using the AND Operator.cs
@@ -0,0 +1,14 @@
+//Consider a && b:
+//a is checked if it is true or false.
+//If a is false, false is returned.
+//b is checked if it is true or false.
+//If b is false, false is returned.
+//Otherwise, true is returned (as both a and b are therefore true ).
+//Rembember that the default value for bool is false. (eg. var c = new bool(); c is false.)
+//The && operator will only return true for true && true.
+//Make a function using the && operator.
+using System;
+public class Program17
+{
+	public static bool And(bool x, bool y)=>x&&y;
+}
