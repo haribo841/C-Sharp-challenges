a0884ac48255cd11d2848e1422a25e6a2474ea02
Adam Kubiś <adamwaldemarkubis@gmail.com>
2023-01-25 02:26:47 +0100

    Fix20
    Fixed test 20

---
+++ VeryEasy/20 Name Greeting!.cs
@@ -0,0 +1,6 @@
+//Create a function that takes a name and returns a greeting in the form of a string.
+using System;
+public class Program20
+{
+	public static string HelloName(string name) => "Hello "+name+"!";
+}
