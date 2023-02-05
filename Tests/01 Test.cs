89848066e63c9e39c9f9666d2300a45c13ae6107
Adam Kubiś <adamwaldemarkubis@gmail.com>
2023-02-05 22:24:48 +0100

    Fix24
    Fixed test 24

--- Tests/01 Test.cs
+++ Tests/01 Test.cs
@@ -14,20 +14,5 @@ namespace VeryEasy.Tests
             int result = Program1.Sum(a, b);// Act
             Assert.That(result, Is.EqualTo(expectedResult));
         }
-        /*         [Test]
-        [TestCase(2, 3, 5, TestName = nameof(FormatTestName))]
-        [TestCase(-3, -6, -9, TestName = nameof(FormatTestName))]
-        [TestCase(7, 3, 10, TestName = nameof(FormatTestName))]
-        public void FixedTest(int a, int b, int expectedResult)
-        {
-            int result = Program1.Sum(a, b);// Act
-            Assert.That(result, Is.EqualTo(expectedResult));
-        }
-        [MethodImpl(MethodImplOptions.NoInlining)]  
-        private string FormatTestName(int a, int b, int c)
-        {
-            return $"{a} plus {b} should be {c}";
-        }
-        NUnit3TestAdapter will not display the content of FormatTestName*/
     }
 }
---
+++ VeryEasy/24 Hours and Minutes into Seconds.cs
@@ -0,0 +1,6 @@
+//Write a function that takes two integers (hours, minutes), converts them to seconds, and adds them.
+using System;
+public class Program24
+{
+  public static int Convert(int hours, int minutes)=>hours*3600+minutes*60;
+}
