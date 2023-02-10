de836af302581ef62fcb2796cc6e0b56993beef6
Adam Kubiś <adamwaldemarkubis@gmail.com>
2023-02-10 21:40:40 +0100

    Partialal28
    Fixed test 28 but challenge don't pass every test

---
+++ VeryEasy/28 Integer is Divisible By Five.cs
@@ -0,0 +1,6 @@
+//Create a function that returns true if an integer is evenly divisible by 5, and false otherwise.
+using System;
+public class Program28
+{
+    public static bool divisibleByFive(int n) => n / 5 == 1;
+}
