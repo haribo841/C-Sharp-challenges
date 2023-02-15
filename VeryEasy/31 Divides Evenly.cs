2aff8cb7ddad8ecce1073fc89ce99565f14f12b9
Adam Kubiś <adamwaldemarkubis@gmail.com>
2023-02-15 23:47:56 +0100

    Fix31
    Fixed test 31

---
+++ VeryEasy/31 Divides Evenly.cs
@@ -0,0 +1,6 @@
+//Given two integers, a and b, return true if a can be divided evenly by b. Return false otherwise.
+using System;
+public class Program31
+{
+	public static bool dividesEvenly(int a, int b) => a % b == 0;
+}
