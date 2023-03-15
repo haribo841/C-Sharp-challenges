568e8b67cfbe99fbe4e6d0522dc73095206e3d0a
haribo841 <adamwaldemarkubis@gmail.com>
2023-03-15 17:02:13 +0100

    Fix40

---
+++ Challenges/40 Nth Even Number.cs
@@ -0,0 +1,6 @@
+//Create a function that takes a number n and returns the nth even number beginning with 0 as the first.
+using System;
+public class Program40
+{
+    public static int NthEven(int n) => 2 * n - 2;
+}
