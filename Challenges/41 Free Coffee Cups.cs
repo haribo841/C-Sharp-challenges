97fa4f59596a14f684a3c871028033e364cca2a2
Adam Kubiś <haribo841@gmail.com>
2023-03-17 00:48:42 +0100

    Fix41

---
+++ Challenges/41 Free Coffee Cups.cs
@@ -0,0 +1,7 @@
+//For each of the 6 coffee cups I buy, I get a 7th cup free. In total, I get 7 cups.
+//Create a function that takes n cups bought and return the total number of cups I would get.
+using System;
+public class Program41
+{
+	public static int TotalCups(int n) => n / 6 + n;
+}
--- Tests/05 Test.cs
+++ Tests/05 Test.cs
@@ -1,4 +1,3 @@
-//Create a function that takes the age in years and returns the age in days.
 using System;
 using NUnit.Framework;
 
