86c55a8786cef6d3d891d3e769f0fd85c63af0f0
haribo841 <adamwaldemarkubis@gmail.com>
2023-04-12 18:46:31 +0200

    Fix63

---
+++ Challenges/63 Add, Subtract, Multiply or Divide.cs
@@ -0,0 +1,22 @@
+//Write a function that takes two numbers and returns if they should be added, subtracted, multiplied or divided to get 24.
+//If none of the operations can give 24, return "none".
+using System;
+using System.Linq.Expressions;
+
+namespace Challenges
+{
+    public class Program63
+    {
+        public static string Operation(int num1, int num2)
+        {
+            return (24) switch
+            {
+                int n when n == num1 + num2 => "added",
+                int n when n == num1 - num2 => "subtracted",
+                int n when n == num1 * num2 => "multiplied",
+                int n when n == num1 / num2 => "divided",
+                _ => "none"
+            };
+        }
+    }
+}
