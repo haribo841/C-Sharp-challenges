1e92c3224e8f003757218a200e40a761a93bb5fd
haribo841 <adamwaldemarkubis@gmail.com>
2023-04-19 23:58:34 +0200

    aaaa

---
+++ Challenges/70 Case Insensitive Comparison.cs
@@ -0,0 +1,11 @@
+//Write a function that validates whether two strings are identical and make it case insensitive.
+using System;
+namespace Challenges
+{
+    public class Program70
+    {
+        public static bool match(string s1, string s2)
+    {
+
+    }
+}
