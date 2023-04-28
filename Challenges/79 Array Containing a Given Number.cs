2589d3c70f0f50522717da214b594b2edacd202c
haribo841 <adamwaldemarkubis@gmail.com>
2023-04-28 20:41:34 +0200

    Fix79

---
+++ Challenges/79 Array Containing a Given Number.cs
@@ -0,0 +1,11 @@
+//Write a method to check if an array contains a particular number.
+using System;
+using System.Text;
+
+namespace Challenges
+{
+    public class Program79
+    {
+        public static bool Check(int[] arr, int el) => arr.Contains(el);
+    }
+}
