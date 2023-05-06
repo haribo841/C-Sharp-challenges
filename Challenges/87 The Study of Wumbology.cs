4f9879fdb60dae93f83334e7bdaac116eaf9ff96
haribo841 <adamwaldemarkubis@gmail.com>
2023-05-06 12:48:58 +0200

    Fix87

---
+++ Challenges/87 The Study of Wumbology.cs
@@ -0,0 +1,10 @@
+//Create a function that flips M's to W's (all uppercase).
+using System;
+using System.Text;
+namespace Challenges
+{
+    public class Program87
+    {
+        public static string Wumbo(string words) => words.Replace('M', 'W');
+    }
+}
