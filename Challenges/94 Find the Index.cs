fccf6a2a0080a72616406db8cd90b357d6139396
haribo841 <adamwaldemarkubis@gmail.com>
2023-05-14 23:17:03 +0200

    Fix95 attempt

--- Challenges/94 Find the Index.cs
+++ Challenges/94 Find the Index.cs
@@ -1,24 +1,13 @@
-//Create a function that takes an array and a string as arguments and returns the index of the string.
-using NUnit.Framework;
+//Create a function that takes a number (from 1 - 60) and returns a corresponding string of hyphens.
 using System;
-
-[TestFixture]
-public class Tests
+using System.Text;
+namespace Challenges
 {
-    [Test]
-    [TestCase(new string[] { "abc", "ghj", "banana", "grape" }, "grape", Result = 3)]
-    [TestCase(new string[] { "a", "b", "c", "d", "e", "f" }, "f", Result = 5)]
-    [TestCase(new string[] { "hi", "edabit.com", "testcase", "validstring" }, "edabit.com", Result = 1)]
-    public static int FindIndex(string[] arr, string str)
+    public class Program94
     {
-        Console.WriteLine($"Input: [{string.Join(", ", arr)}], {str}");
-        return Program.FindIndex(arr, str);
-    }
-}
-public class Program
-{
-    public static int FindIndex(string[] arr, string str)
-    {
-
+        public static int FindIndex(string[] arr, string str)
+        {
+            //todo
+        }
     }
 }
---
+++ Challenges/95 Back to Home.cs
@@ -0,0 +1,8 @@
+using System;
+namespace Challenges
+{
+    public class Program
+    {
+        public static bool ReturnTrue() => true;
+    }
+}
