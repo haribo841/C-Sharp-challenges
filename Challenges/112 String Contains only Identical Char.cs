1a52df8bdf2f5aaa9e58dcd320800fbb3190cd68
haribo841 <adamwaldemarkubis@gmail.com>
2023-05-31 22:19:54 +0200

    Fix112

---
+++ Challenges/112 String Contains only Identical Char.cs
@@ -0,0 +1,16 @@
+//Write a function that returns true if all characters in a string are identical and false otherwise.
+using System;
+
+namespace Challenges
+{
+    public class Program112
+    {
+        public static bool isIdentical(string str)
+        {
+            char first = str[0];
+            foreach (char c in str) { 
+                if (c != first) return false; }
+            return true;
+        }
+    }
+}
