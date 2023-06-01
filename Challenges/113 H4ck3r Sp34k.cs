f6bdf67bb0ba9ee74f802a4dc69aa01a1df41bd6
Adam Kubiś <adamwaldemarkubis@gmail.com>
2023-06-01 23:58:15 +0200

    Fix113 attempt

---
+++ Challenges/113 H4ck3r Sp34k.cs
@@ -0,0 +1,10 @@
+//Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+using System;
+
+namespace Challenges
+{
+    public class Program3
+    {
+        public static int Addition(int num) => ++num;
+    }
+}
---
+++ Tests/113 Test.cs
@@ -0,0 +1,23 @@
+using System;
+using Challenges;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests3
+    {
+        [Test]
+        [TestCase(2, 3, TestName = "{0} incremented by one should be {1}")]
+        [TestCase(-9, -8, TestName = "{0} incremented by one should be {1}")]
+        [TestCase(0, 1, TestName = "{0} incremented by one should be {1}")]
+        [TestCase(100, 101, TestName = "{0} incremented by one should be {1}")]
+        [TestCase(999, 1000, TestName = "{0} incremented by one should be {1}")]
+        [TestCase(73, 74, TestName = "{0} incremented by one should be {1}")]
+        public void FixedTest(int num, int expectedResult)
+        {
+            int result = Program3.Addition(num);
+            Assert.That(result, Is.EqualTo(expectedResult));// Assert
+        }
+    }
+}
