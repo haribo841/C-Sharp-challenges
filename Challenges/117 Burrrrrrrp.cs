567c2b43638917a2d5e2842870ff89aecf9d7977
Adam Kubiś <adamwaldemarkubis@gmail.com>
2023-06-06 23:54:41 +0200

    Fix117 attempt

---
+++ Challenges/117 Burrrrrrrp.cs
@@ -0,0 +1,12 @@
+//Create a function that returns the string "Burp" with the amount of "r's" determined by the input parameters of the function.
+using System;
+namespace Challenges
+{
+    public class Program117
+    {
+        public static string LongBurp(int b)
+        {
+            return "";
+        }
+    }
+}
