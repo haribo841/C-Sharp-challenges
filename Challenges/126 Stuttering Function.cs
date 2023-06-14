a7d9822f3c4236921b4f7b5f86379fd2ffd2f221
Adam Kubiś <adamwaldemarkubis@gmail.com>
2023-06-14 23:55:37 +0200

    Solved 126

---
+++ Challenges/126 Stuttering Function.cs
@@ -0,0 +1,18 @@
+//Write a function that stutters a word as if someone is struggling to read it.
+//The first two letters are repeated twice with an ellipsis ...
+//and space after each, and then the word is pronounced with a question mark ?.
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Challenges
+{
+    public class Program126
+    {
+        public static string Stutter(string word)
+        {
+            string a = word.Substring(0, 2);
+            string b = "... ";
+            return a + b + a + b + word + "?";
+        }
+    }
+}
