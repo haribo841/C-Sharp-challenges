c4a2f69f8300632cd160caf096e571115dd6a2ee
Adam Kubiś <adamwaldemarkubis@gmail.com>
2023-06-18 21:05:30 +0200

    128 attempt

---
+++ Challenges/128 Re-Form the Word.cs
@@ -0,0 +1,10 @@
+//Create a function that takes a number as its only argument and returns true if it's less than or equal to zero, otherwise return false.
+using System;
+
+namespace Challenges
+{
+    public class Program8
+    {
+        public static bool LessThanOrEqualToZero(double a) => a <= 0;
+    }
+}
