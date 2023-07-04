64a773f97ac2fa8721ec366c0350f4e212ab5dfb
Adam Kubiś <adamwaldemarkubis@gmail.com>
2023-07-04 20:51:33 +0200

    141 attempt

---
+++ Challenges/134 Reverse and Capitalize.cs
@@ -0,0 +1,10 @@
+//Create a function that takes voltage and current and returns the calculated power.
+using System;
+
+namespace Challenges
+{
+    public class Program4
+    {
+        public static int CircuitPower(int voltage, int current) => voltage * current;
+    }
+}
---
+++ Challenges/135 Phone Number Formatting.cs
@@ -0,0 +1,10 @@
+//Create a function that takes the age in years and returns the age in days.
+using System;
+
+namespace Challenges
+{
+    public class Program5
+    {
+        public static int CalcAge(int age) => age * 365;
+    }
+}
