769a39f6c3479edaf09061032a42f86fb5fa62d3
Adam Kubiś <adamwaldemarkubis@gmail.com>
2023-10-31 21:46:48 +0100

    Spooky

---
+++ Challenges/157 Largest Swap.cs
@@ -0,0 +1,10 @@
+//Create a function that takes a two-digit number and determines if it's the largest of two possible digit swaps.
+using System;
+
+namespace Challenges
+{
+    public class Program157
+    {
+        public static bool IsEqual(int num1, int num2) => num1 == num2;
+    }
+}
