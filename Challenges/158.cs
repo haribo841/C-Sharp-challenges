814a6dd20c9e29d48ebbcf9e8c7ad02d3f5bbdfc
haribo841 <adamwaldemarkubis@gmail.com>
2023-11-06 18:36:40 +0100

    158 attempt

---
+++ Challenges/158.cs
@@ -0,0 +1,15 @@
+//Given an array of numbers, write a function that returns an array that...
+//Has all duplicate elements removed.
+//Is sorted from least value to greatest value.
+using System;
+
+namespace Challenges
+{
+    public class Program158
+    {
+        public static double[] UniqueSort(double[] arr)
+        {
+            return null;
+        }
+    }
+}
